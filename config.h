230029b2340d1951a666f601c9be93c15b49a5ed
Anil Kumar Shrestha <anilkumarshrestha@yahoo.com>
2018-11-06 11:28:33 +0545

    Add files via upload

--- config.h
+++ config.h
@@ -5,17 +5,23 @@
 #include <GL/glew.h>
 #include <GL/glm/glm.hpp>
 #include <iostream>
+#include <mingw.thread.h>
 #include <string>
 #include "source/GameEngine/GameEngine.hpp"
 #include <GL/glm/gtc/matrix_transform.hpp>
+#include <SFML/system/Time.hpp>
+#include <SFML/system/Clock.hpp>
 //#include <assimp/Importer.hpp>
 //#include <assimp/scene.h>
 //#include <assimp/postprocess.h>
 //#include <vector>
+
 #define Array std::vector
 
+using namespace std;
+
 #ifndef GAME_NAME
-#define GAME_NAME "Gorakshya: The Brave Leads The Way" //name for the window
+#define GAME_NAME "Army Game"
 #endif
 
 #ifdef _cplusplus
@@ -26,3 +32,5 @@ extern "C"{
 #ifdef _cplusplus
 }
 #endif
+
+int TIME_PER_FRAME = 34;//17 for 60fps
